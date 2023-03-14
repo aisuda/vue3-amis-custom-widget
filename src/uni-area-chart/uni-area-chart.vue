6ad76bb52d748353f0d6e402ce5293997fea03ba
wibetter <365533093@qq.com>
2023-03-14 10:24:24 +0800

    fix: 升级依赖

--- src/uni-area-chart/uni-area-chart.vue
+++ src/uni-area-chart/uni-area-chart.vue
@@ -11,33 +11,27 @@
   </div>
 </template>
 <script>
-import uCharts from '@qiun/ucharts';
+import uCharts from "@qiun/ucharts";
 // 图表详细配置说明请见：https://www.ucharts.cn/v2/#/guide/index
 var uChartsInstance = {};
 export default {
-  props: {
-    categories: {
-      type: Array
-    },
-    series: {
-      type: Array
-    },
-  },
   data() {
     return {
+      categories: [],
+      series: [],
       defaultChartData: {
-        categories: ['2016', '2017', '2018', '2019', '2020', '2021'],
+        categories: ["2016", "2017", "2018", "2019", "2020", "2021"],
         series: [
           {
-            name: '成交量A',
+            name: "成交量A",
             data: [35, 8, 25, 37, 4, 20],
           },
           {
-            name: '成交量B',
+            name: "成交量B",
             data: [70, 40, 65, 100, 44, 68],
           },
           {
-            name: '成交量C',
+            name: "成交量C",
             data: [100, 80, 95, 150, 112, 132],
           },
         ],
@@ -46,19 +40,17 @@ export default {
   },
   computed: {
     chartId() {
-      return this.id || 'area-uChart';
+      return this.id || "area-uChart";
     },
     thisCategories() {
-      return (
-        this.categories ||
-        this.defaultChartData.categories
-      );
+      return this.categories && this.categories.length > 0
+        ? this.categories
+        : this.defaultChartData.categories;
     },
     thisSeries() {
-      return (
-        this.series ||
-        this.defaultChartData.series
-      );
+      return this.series && this.series.length > 0
+        ? this.series
+        : this.defaultChartData.series;
     },
   },
   mounted() {
@@ -69,6 +61,7 @@ export default {
   },
   updated() {
     // 确保编辑器端，右侧属性配置面板调整后图表会及时更新
+    console.log("this.thisSeries:", this.thisSeries);
     this.drawCharts(this.chartId, {
       categories: this.thisCategories,
       series: this.thisSeries,
@@ -79,16 +72,16 @@ export default {
       const canvas = document.getElementById(id) || this.$refs.uchart;
       canvas.width = canvas.offsetWidth;
       canvas.height = canvas.offsetHeight;
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       uChartsInstance[id] = new uCharts({
-        type: 'area',
+        type: "area",
         context: ctx,
         width: canvas.width,
         height: canvas.height,
         categories: data.categories,
         series: data.series,
         animation: true,
-        background: '#ffffff',
+        background: "#ffffff",
         padding: [15, 15, 0, 5],
         xAxis: {
           disableGrid: true,
@@ -97,20 +90,20 @@ export default {
           data: [{ min: 0 }],
         },
         color: [
-          '#1890FF',
-          '#91CB74',
-          '#FAC858',
-          '#EE6666',
-          '#73C0DE',
-          '#3CA272',
-          '#FC8452',
-          '#9A60B4',
-          '#ea7ccc',
+          "#1890FF",
+          "#91CB74",
+          "#FAC858",
+          "#EE6666",
+          "#73C0DE",
+          "#3CA272",
+          "#FC8452",
+          "#9A60B4",
+          "#ea7ccc",
         ],
         legend: {},
         extra: {
           area: {
-            type: 'straight',
+            type: "straight",
             opacity: 0.2,
             addLine: true,
             width: 2,
